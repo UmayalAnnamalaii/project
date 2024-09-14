20d2079127014bc232baf137685ebdb2cab8e754
UmayalAnnamalaii <umayalan12012001@gmail.com>
2024-09-14 12:07:42 +0530

    message

---
+++ src/test/java/com/test/Test.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Test {
+private void tc1() {
+	System.err.println(1);
+
+}
+private void tc2() {
+	System.out.println(2);
+
+}private void tc3() {
+	System.out.println(3);
+
+}
+}
